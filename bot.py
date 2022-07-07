afcc78f1faf67d5d127ab4f98ad24f888b1680f6
aguerot <aguerot@pharmax.fr>
2022-07-07 16:57:44 +0200

    added a few commands for testing, role and guilds id and started to work on add role (does not work ...)

--- bot.py
+++ bot.py
@@ -15,6 +15,11 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+#guild_id=970053878412345374
+#potato=970064386662228018
+guild_id=868573364762054697
+potato=994611463132020846
+ephemeral=False
 # headers = {
 #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
 # }
@@ -37,16 +42,48 @@ from bs4 import BeautifulSoup
 # exit(0)
 
 con = sqlite3.connect('lalabot.db')
-client = pyxivapi.XIVAPIClient(api_key="XIV API TOKEN")
-bot = interactions.Client(token="DISCORD API TOKEN")
+client = pyxivapi.XIVAPIClient(api_key="37a8188ee522466d9eab22ce37f66214293f180b9ac041fc983db0981d0ecb66")
+bot = interactions.Client(token="OTkzODE1NTgwODgyNzYzODI2.G9-4dc.MzseGMR_18RKfqr83Ioa4l9fayf9auDLxxFQtA")
 
 # cur = con.cursor()
 # cur.execute("CREATE TABLE user_verification (discord_id, lodestone_id, token, verified)")
 # con.commit()
 
 @bot.command(
+    name="addpotato",
+    description="Add potato rule to user",
+    default_member_permissions=interactions.Permissions.ADMINISTRATOR,
+    scope=guild_id,
+    options = [
+        interactions.Option(
+            name="user",
+            description="User",
+            type=interactions.OptionType.USER,
+            required=True,
+        ),
+    ]
+)
+async def addpotato_command(ctx: interactions.CommandContext, user: interactions.Member):
+    guild = await ctx.get_guild()
+    await user.add_role(guild.id, potato)
+    await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving potato role")
+
+@bot.command(
+    name="clearlaladb",
+    description="Clear the lala registration DB",
+    default_member_permissions=interactions.Permissions.ADMINISTRATOR,
+    scope=guild_id
+)
+async def clearlaladb_command(ctx: interactions.CommandContext):
+    cur = con.cursor()
+    cur.execute("DELETE FROM user_verification")
+    con.commit()
+    await ctx.send("Done.", ephemeral=ephemeral)
+
+@bot.command(
     name="iamlala",
     description="Register yourself as a Lala and get automatically verified.",
+    scope=guild_id,
     options = [
         interactions.Option(
             name="world",
@@ -82,7 +119,7 @@ async def iamlala_command(ctx: interactions.CommandContext, world: str, forename
         await ctx.send("No character found :(")
         return
     elif len(characterSearchResult['Results']) > 1:
-        await ctx.send("Too many characters found for this search -_-", ephemeral=True)
+        await ctx.send("Too many characters found for this search -_-", ephemeral=ephemeral)
         return
     
     lodesteoneId = characterSearchResult['Results'][0]['ID']
@@ -95,7 +132,7 @@ async def iamlala_command(ctx: interactions.CommandContext, world: str, forename
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     }
-    url = "https://fr.finalfantasyxiv.com/lodestone/character/{}/".format(lodesteoneId)
+    url = "https://na.finalfantasyxiv.com/lodestone/character/{}/".format(lodesteoneId)
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, features="lxml")
     prettyHTML = soup.prettify() 
@@ -157,17 +194,17 @@ async def iamlala_command(ctx: interactions.CommandContext, world: str, forename
         ## yes, they do so we can register them (if they are lala :p)
         if hasToken:
             if not isLala:
-                await ctx.send("No no no ! Only lalas there !", ephemeral=True)
+                await ctx.send("No no no ! Only lalas there !", ephemeral=ephemeral)
                 return
             else:
                 cur.execute("UPDATE user_verification SET verified=? WHERE discord_id=?", (True, str(discordId)))
                 con.commit()
-                await ctx.send("Congratulations, your account have been verified ! Welcome to Lalaland", ephemeral=True)
+                await ctx.send("Congratulations, your account have been verified ! Welcome to Lalaland", ephemeral=ephemeral)
                 return
 
         ## no, we tell them their tokens
         else:
-            await ctx.send("Hey, i can't register your character :(\nYou need to put this string into your lodestone bio:\n``{}``".format(token), ephemeral=True)
+            await ctx.send("Hey, i can't register your character :(\nYou need to put this string into your lodestone bio:\n``{}``".format(token), ephemeral=ephemeral)
             return
 
     ##################
@@ -175,7 +212,5 @@ async def iamlala_command(ctx: interactions.CommandContext, world: str, forename
     else:
         await ctx.send("Your character is already registered.")
         return
-
-    # await ctx.send("isLala={}, bioLines={}, token={}".format(isLala, bioLines, authToken), ephemeral=True)    
     
 bot.start()
